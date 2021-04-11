2c1b17d39c8015460c71eb065345eb05ae055759
DesmondM <desmondmpofu@gmail.com>
2021-04-11 19:41:34 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Review from './Review';
+
+function App() {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          
+          <img src= "/images/hopefountain4.png"/>
+          
+          <h2>Hope Fountain Mission </h2>
+          <div className='underline'></div>
+          <h3>-</h3>
+          <h3>Cemetery Cleanup Initiative </h3>
+          <h3>24 April 2021 </h3>
+          <h4>Contact: <a href="tel:+263 77 289 0879">+263 77 289 0879 </a> </h4>
+        </div>
+        <Review />
+         
+         
+      </section>
+    </main>
+  );
+}
+
+export default App;
+ 
---
+++ src/data.js
@@ -0,0 +1,50 @@
+
+const reviews = [
+  {
+    id: 1,
+    name: 'Cemetery',
+    job: '',
+    image:
+      './images/hopefountain.jpeg',
+    text:
+      "Please join us for the Hope Fountain Cemetery clean up event on the  24th of April 2021 from 6am. We hope to clean up the gravesite, it is now mostly covered in grass, weeds and shrubs",
+  },
+  {
+    id: 2,
+    name: 'Hope Fountain Mission Church',
+    job: 'The Church',
+    image:
+      './images/hopefountain2.jpeg',
+    text:
+      "Please join us for the Hope Fountain Cemetery clean up event on the  24th of April 2021 from 6am. We hope to clean up the gravesite, it is now mostly covered in grass, weeds and shrubs",
+  },
+  {
+    id: 3,
+    name: 'The Cemetery',
+    job: 'Current State of the Cemetery',
+    image:
+      './images/hopefountain3.jpeg',
+    text:
+      "Please join us for the Hope Fountain Cemetery clean up event on the  24th of April 2021 from 6am. We hope to clean up the gravesite, it is now mostly covered in grass, weeds and shrubs",
+  },
+  {
+    id: 4,
+    name: 'The Cemetery',
+    job: 'Current State of the Cemetery',
+    image:
+      './images/hopefountain7.jpeg',
+    text:
+      "Please join us for the Hope Fountain Cemetery clean up event on the  24th of April 2021 from 6am. We hope to clean up the gravesite, it is now mostly covered in grass, weeds and shrubs",
+  },
+  {
+    id: 4,
+    name: 'The Cemetery',
+    job: 'Grass and bushes covering graves',
+    image:
+      './images/hopefountain5.jpeg',
+    text:
+     "Please join us for the Hope Fountain Cemetery clean up event on the  24th of April 2021 from 6am. We hope to clean up the gravesite, it is now mostly covered in grass, weeds and shrubs",
+  },
+];
+
+export default reviews;
